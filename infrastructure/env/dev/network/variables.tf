0c969de658caa3349890e4747efca1dd14414d71
nao <na0k1ura6e@gmail.com>
2026-04-11 17:38:56 +0900

    WIP: add network setting

---
+++ infrastructure/env/dev/network/variables.tf
@@ -0,0 +1,15 @@
+variable "vpc_cidr" {
+  type = string
+  description = "(Required) VPC vpc_cidr"
+}
+
+variable "vpc_name" {
+  type = string
+  description = "(Required) VPC Name"
+}
+
+variable "env" {
+  type = string
+  description = "(Required) Environment name"
+}
+
